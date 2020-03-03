b044b63f9b9395f8fb6ab6f8fe3d978bf0116343
dev-rm <54745323+dev-rm@users.noreply.github.com>
2020-03-03 06:38:04 +0530

    Add comments

--- src/singleton/ThreadSafeSingletonDemo.java
+++ src/singleton/ThreadSafeSingletonDemo.java
@@ -13,8 +13,8 @@ public class ThreadSafeSingletonDemo {
 	
 	public static ThreadSafeSingletonDemo getInstance() {
 		if(instance == null) {
-			synchronized(ThreadSafeSingletonDemo.class) {
-				if(instance == null) {
+			synchronized(ThreadSafeSingletonDemo.class) { //The instance may be null but when we synchronize it and check for null again and if another class has a lock on that it will then block our code and create the instance and return that synchronized lock to our code and then it checks for null again and since it's not null it doesn't need to create an instance of the class again.
+				if(instance == null) { //It will run only when we are creating an instance for the very first time
 					instance = new ThreadSafeSingletonDemo();
 				}
 			}
